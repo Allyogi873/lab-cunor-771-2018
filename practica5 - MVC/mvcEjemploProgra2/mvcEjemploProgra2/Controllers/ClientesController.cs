5a962bf614168bc3de8312f3f353bcc91ef3dacb
Geovani de León <egdeleon@pensotec.com>
2018-09-25 11:25:49 -0600

    Práctica con MVC, C#, Entity Framework y Linq

---
+++ practica5 - MVC/mvcEjemploProgra2/mvcEjemploProgra2/Controllers/ClientesController.cs
@@ -0,0 +1,99 @@
+﻿using mvcEjemploProgra2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvcEjemploProgra2.Controllers
+{
+    public class ClientesController : Controller
+    {
+        mdlFacturacion ctx = new mdlFacturacion();
+        // GET: Clientes
+        public ActionResult Index()
+        {
+            var query = from c in ctx.cliente
+                        select c;
+
+            ViewBag.lstClientes = query.ToArray();
+
+            return View();
+        }
+
+        // GET: Clientes/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Clientes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Clientes/Create
+        [HttpPost]
+        public ActionResult Create(cliente registro)
+        {
+            try
+            {
+                ctx.cliente.Add(registro);
+                ctx.SaveChanges();
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                ctx.Dispose();
+                return View();
+            }
+        }
+
+        // GET: Clientes/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Clientes/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Clientes/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Clientes/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ practica5 - MVC/mvcEjemploProgra2/mvcEjemploProgra2/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvcEjemploProgra2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult HolaMundo() {
+            List<Persona> lstPersonas = new List<Persona>();
+            lstPersonas.Add(new Persona("1", "Jonathan Ramirez"));
+            lstPersonas.Add(new Persona("2", "Angel Ruiz"));
+            lstPersonas.Add(new Persona("3", "David Montoya"));
+
+            ViewBag.items = lstPersonas.ToArray();
+
+            return View();
+        }
+
+        public class Persona {
+            public Persona(string no_persona, string nombre) {
+                this.nombre = nombre;
+                this.no_persona = no_persona;
+            }
+
+            public string no_persona { get; set; }
+            public string nombre { get; set; }
+        }
+    }
+}
