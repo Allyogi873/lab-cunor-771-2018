68c910ab74ee6fff8980ef3e994364e751af626f
Geovani de León <egdeleon@pensotec.com>
2018-10-12 11:42:11 -0600

    C# Maestro Detalle

---
+++ practica5 - MVC/mvcEjemploProgra3/mvcEjemploProgra3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="mvcEjemploProgra3.MvcApplication" Language="C#" %>
---
+++ practica5 - MVC/waPrueba5/waPrueba5/Controllers/FacturasController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using waPrueba5.Models;
+
+namespace waPrueba5.Controllers
+{
+    public class FacturasController : Controller
+    {
+        ctxPruebas ctx = new ctxPruebas();
+        // GET: Facturas
+        public ActionResult Index()
+        {
+            var query = from f in ctx.factura
+                        select f;
+
+
+            return View(query.ToList());
+        }
+
+        // GET: Facturas/Details/5
+        public ActionResult Details(int id)
+        {
+            factura query = ctx.factura.Find(id);
+            return View(query);
+        }
+
+        // GET: Facturas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Facturas/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Facturas/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Facturas/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Facturas/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Facturas/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
