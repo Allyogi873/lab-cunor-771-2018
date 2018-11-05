25e07b73bab3983dab9cbd8d7b037e00ff797635
Geovani de León <egdeleon@pensotec.com>
2018-11-05 13:02:15 -0600

    Se agregó registro de notas.

--- practica5 - MVC/waPruebaLogin/waPruebaLogin/Controllers/SeccionesController.cs
+++ practica5 - MVC/waPruebaLogin/waPruebaLogin/Controllers/SeccionesController.cs
@@ -21,7 +21,8 @@ namespace waPruebaLogin.Controllers
             return View(query.ToList());
         }
 
-        public ActionResult ListaActividades(int id) {
+        public ActionResult ListaActividades(int id)
+        {
             var query = from m in ctx.actividad
                         where m.cod_seccion == id
                         select m;
@@ -30,32 +31,37 @@ namespace waPruebaLogin.Controllers
             return View(query.ToList());
         }
 
-        public ActionResult NuevaActividad(int id) {
+        public ActionResult NuevaActividad(int id)
+        {
             ViewBag.seccion = id;
             return View();
         }
 
         [HttpPost]
-        public ActionResult NuevaActividad(actividad registro) {
+        public ActionResult NuevaActividad(actividad registro)
+        {
 
-            try {
+            try
+            {
                 ViewBag.seccion = registro.cod_seccion;
                 seccion c = ctx.seccion.Find(registro.cod_seccion);
                 registro.cod_curso = c.cod_curso;
                 ctx.actividad.Add(registro);
                 ctx.SaveChanges();
             }
-            catch (Exception ex) {
+            catch (Exception ex)
+            {
                 return View();
             }
             return RedirectToAction("ListaActividades", new { id = registro.cod_seccion });
         }
 
-        public ActionResult Notas(int id) {
+        public ActionResult Notas(int id)
+        {
             var estudiantes = (from m in ctx.det_seccion
-                              join es in ctx.estudiante on m.cod_estudiante equals es.cod_estudiante
-                              where m.cod_seccion == id
-                              select new {Codigo = es.cod_estudiante, Carne = es.carne, Nombre = es.nombre_completo}).ToList<Object>();
+                               join es in ctx.estudiante on m.cod_estudiante equals es.cod_estudiante
+                               where m.cod_seccion == id
+                               select es);
 
             ViewBag.estudiantes = estudiantes;
 
@@ -64,7 +70,37 @@ namespace waPruebaLogin.Controllers
                               select m;
 
             ViewBag.actividades = actividades.ToList();
+
+            seccion sec = ctx.seccion.Find(id);
+            ViewBag.cod_seccion = sec.cod_seccion;
+            ViewBag.cod_curso = sec.cod_curso;
+
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Notas(List<nota> notas)
+        {
+            foreach (var itm in notas) {
+                nota regAnterior = (from m in ctx.nota
+                                    where m.cod_curso == itm.cod_curso &&
+                                    m.cod_seccion == itm.cod_seccion &&
+                                    m.cod_estudiante == itm.cod_estudiante &&
+                                    m.cod_actividad == itm.cod_actividad
+                                    select m).FirstOrDefault();
+
+                if (regAnterior != null)
+                {
+                    regAnterior.nota1 = itm.nota1;
+                    ctx.Entry(regAnterior).State = System.Data.Entity.EntityState.Modified;
+                }
+                else {
+                    ctx.nota.Add(itm);
+                }
+            }
+            ctx.SaveChanges();
+
+            return RedirectToAction("Index");
         }
     }
 }
